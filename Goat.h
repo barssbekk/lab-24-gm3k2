97dfc68cda0239e00a53b05edb70088af199ced1
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-05 19:36:28 -0700

    first commit

---
+++ Goat.h
@@ -0,0 +1,47 @@
+// Goat.h
+
+#ifndef GOAT_H
+#define GOAT_H
+#include <iostream>
+using namespace std;
+
+class Goat {
+private:
+    string name;
+    int age;
+    string color;
+public:
+    Goat() { name = ""; age = 0; color = ""; }
+
+    // write three more constructors
+    Goat(const string& userName) {
+        name = userName;
+        age = 0;
+        color = "";
+    }
+    Goat(const string& userName, const int userAge) {
+        name = userName;
+        age = userAge;
+        color = "";
+    }
+    Goat(const string& userName, const int userAge, const string& userColor) {
+        name = userName;
+        age = userAge;
+        color = userColor;
+    }
+
+    // setters and getters
+    void set_name(string n)         { name = n; };
+    string get_name() const         { return name; };
+    void set_age(int a)             { age = a; };
+    int get_age() const             { return age; }
+    void set_color(string c)        { color = c; }
+    string get_color() const        { return color; }
+
+    // write overloaded < operator for the std::list
+    bool operator < (const Goat& other) const {
+        return name < other.name;
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include <ctime>
+#include "Goat.h"
+using namespace std;
+
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20, MIN_AGE = 1;
+
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string [], string [], int, int);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+int main() {
+    srand(time(0));
+    bool again = true;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int nameCount = 0;
+    while (nameCount < SZ_NAMES && fin >> names[nameCount]) {
+        ++nameCount;
+    }
+
+    fin.close();
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    int colorCount = 0;
+    while (colorCount < SZ_COLORS && fin1 >> colors[colorCount]) {
+        ++colorCount;
+    }
+
+    fin1.close();
+    list<Goat> trip;
+    while (again) {
+        int choice = main_menu();
+
+        switch (choice) {
+            case 1:
+                add_goat(trip, names, colors, nameCount, colorCount);
+                break;
+            case 2:
+                delete_goat(trip);
+                break;
+            case 3:
+                display_trip(trip);
+                break;
+            case 4:
+                again = false;
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void add_goat(list<Goat>& trip, string names[], string colors[],
+              int nameCount, int colorCount) {
+    string rndName = names[rand() % nameCount];
+    string rndColors = colors[rand() % colorCount];
+    int rndAge = rand() % (MAX_AGE - MIN_AGE + 1) + MIN_AGE;
+    trip.push_back(Goat(rndName, rndAge, rndColors));
+    trip.sort();
+}
+
+int select_goat(list<Goat> trip) {
+    int index = 1;
+
+    for (Goat goat : trip) {
+        cout << "[" << index++ << "] "
+             << goat.get_name()
+             << " (" << goat.get_age()
+             << ", " << goat.get_color()
+             << ")\n";
+    }
+
+    int choice;
+    cout << "Choice --> ";
+    cin >> choice;
+
+    while (choice < 1 || choice >= index) {
+        cin.clear();
+        cin.ignore();
+        cout << "Choice --> ";
+        cin >> choice;
+    }
+    return choice;
+}
+
+// TODO: delete goat
+void delete_goat(list<Goat>& trip) {
+    if (trip.empty()) return;
+    int choice = select_goat(trip);
+
+    auto it = trip.begin();
+    advance(it, choice - 1);
+    trip.erase(it);
+}
+
+// TODO: print goat
+void display_trip(list<Goat> trip) {
+    int index = 1;
+    for (Goat g : trip) {
+        cout << "[" << index++ << "] "
+             << g.get_name()
+             << " (" << g.get_age()
+             << ", " << g.get_color()
+             << ")\n";
+    }
+}
+
+int main_menu() {
+    int choice = 0;
+    cout << "*** GOAT MANAGER 3001 ***\n"
+         << "[1] Add a goat\n"
+         << "[2] Delete a goat\n"
+         << "[3] List goats\n"
+         << "[4] Quit\n"
+         << "Choice --> ";
+    cin >> choice;
+    while (choice < 1 || choice > 4) {
+        cerr << "Invalid input. Try again: ";
+        cin >> choice;
+    }
+    return choice;
+}
+
